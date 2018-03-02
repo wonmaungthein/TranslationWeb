a20998a4ac2411e6a5e4393dd1f9f87f402d9aa5
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-02 22:02:25 +0000

    installed builder package, created footer and styled a bit

--- translation/src/App.js
+++ translation/src/App.js
@@ -2,17 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const footerstyles = {
+  background: 'lightblue',
+  paddingTop: 80,
+  paddingBottom: 80
+}
+
+const sectionstyles = {
+  background: 'orange',
+  paddingTop: 80,
+  paddingBottom: 80
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Translation Web Site</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <section style={sectionstyles}>
+          This part will be a section part.
+          </section>
+        <footer style={footerstyles} >This part will be a footer part.
+            </footer>
       </div>
     );
   }
